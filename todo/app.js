91fb0ebe90142ca5f8c845fbf655b3eadcbafa09
Eduardo Romero <eduardoromeror9@outlook.com>
2022-03-26 10:09:47 -0300

    App de tareas terminadagit log --oneline

--- todo/app.js
+++ todo/app.js
@@ -1,7 +1,14 @@
 require('colors');
 
 const { guardarDb, leerDb } = require('./helpers/guardarArchivo');
-const { inquirerMenu, pausa, leerInput } = require('./helpers/inquirer');
+const { inquirerMenu, 
+  pausa,
+  leerInput,
+  listadoTareasBorrar,
+  confirmar,
+  mostrarListadoCheckList 
+} = require('./helpers/inquirer');
+
 const Tareas = require('./models/tareas');
 
 
@@ -38,6 +45,23 @@ const main = async () => {
       
       case '4':
         tareas.listarPendientesCompletadas(false);
+      break;
+
+      case '5':
+        const ids = await mostrarListadoCheckList(tareas.listadoArray);
+        tareas.toogleCompletadas(ids);
+      break;
+
+      case '6':
+        const id = await listadoTareasBorrar( tareas.listadoArray );
+
+        if ( id !== '0' ) {
+          const confirmacion = await confirmar(`Estas seguro que deseas eliminar la tarea ${id}?`);
+        if ( confirmacion ) {
+          tareas.borrarTarea(id);
+          console.log('Tarea eliminada');
+        }
+      }   
       break;
     }
 
