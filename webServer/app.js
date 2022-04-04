b346a9c752d6b084ed3b9a77151accf5ca4df3dc
Eduardo Romero <eduardoromeror9@outlook.com>
2022-04-04 06:13:39 -0400

    webServer dia 3

--- webServer/app.js
+++ webServer/app.js
@@ -1,19 +1,47 @@
 const express = require('express')
+const hbs = require('hbs')
+require('dotenv').config();
+
 const app = express()
-const port = 8080
+const port = process.env.PORT;
+
+
+// Handlebars
+app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+// Serve static files
+app.use(express.static('public'))
+
 
 app.get('/', (req, res) => {
-  res.send('Home Page')
+  res.render('home', {
+    nombre: ' Eduardo Romero',
+    titulo: 'Node.js'
+  });
 })
 
 
-app.get('/hola-mundo', (req, res) => {
-  res.send('Hello World en su ruta')
+
+app.get('/generic', (req, res) => {
+  res.render('generic', {
+    nombre: ' Eduardo Romero',
+    titulo: 'Node.js'
+  });
+})
+
+
+app.get('/elements', (req, res) => {
+  res.render('elements', {
+    nombre: ' Eduardo Romero',
+    titulo: 'Node.js'
+  });
 })
 
 
 app.get('*', function (req, res) {
-  res.send('404 | Page not found')
+  res.sendFile(__dirname + '/public/404.html');
 })
 
 
